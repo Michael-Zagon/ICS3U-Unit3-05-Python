fca0fddb63da4258c73681bb7f8cbd78a9fa07b6
Ubuntu <ubuntu@ip-172-31-7-8.ec2.internal>
2021-09-22 16:49:12 +0000

    month number python

---
+++ month.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Zagon
+# Created on: Sep 2021
+# This program tells a user what month is represented by their number
+
+
+def main():
+    # This function determines what month their number represents
+
+    # Input
+
+    month_number = int(input("Enter the number of a month 1-12: "))
+    print("")
+
+    # Process and Output
+    if month_number == 1:
+        print("January")
+    elif month_number == 2:
+        print("February")
+    elif month_number == 3:
+        print("March")
+    elif month_number == 4:
+        print("April")
+    elif month_number == 5:
+        print("May")
+    elif month_number == 6:
+        print("June")
+    elif month_number == 7:
+        print("July")
+    elif month_number == 8:
+        print("August")
+    elif month_number == 9:
+        print("September")
+    elif month_number == 10:
+        print("October")
+    elif month_number == 11:
+        print("November")
+    elif month_number == 12:
+        print("December")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
